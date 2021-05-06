339eebb225ee818757dfedbc72eb378ab51a8bf0
zilongshanren <guanghui8827@126.com>
2021-05-06 11:54:57 +0800

    modify emacs

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-hugo --theme=even --baseUrl="https://zilongshanren.com"
-hugo && rsync -avz --delete public/ root@112.124.58.18:/var/www/octopress-cn
+hugo --theme=even --baseUrl="https://www.emacs-china.org"
+hugo && rsync -avz --delete public/ root@112.124.58.18:/var/www/emacs-china.org
